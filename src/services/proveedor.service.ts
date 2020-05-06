75bdb80797e3f2a29d5a67f09e98d137a80db109
sergiooso <60956088+sergiooso@users.noreply.github.com>
2020-05-06 04:07:34 -0600

    creacion de los endpoints actualizar y optener un proveedor

--- src/services/proveedor.service.ts
+++ src/services/proveedor.service.ts
@@ -1,5 +1,5 @@
 import{Request,Response} from "express";
-import {Proveedor} from "../Modelos/proveedor.Modelos";
+import {Proveedor, IProveedor} from "../Modelos/proveedor.Modelos";
 import { MongooseDocument } from "mongoose";
  
 export class ProveedorService{
@@ -11,5 +11,21 @@ export class ProveedorService{
              }
              res.status(200).json(proveedores);
          });
+    }
+    public getid(req:Request,res:Response){
+        Proveedor.findById(req.params.id,(err:Error,proveedor:IProveedor)=>{
+            if(err){
+                res.status(401).send(err); 
+            }
+            res.status(200).json(proveedor? proveedor:{});
+        });
+    }
+    public actualizar(req:Request,res:Response){
+        Proveedor.findByIdAndUpdate(req.params.id,req.body,(err:Error,proveedor:any)=>{
+          if(err){
+              res.status(401).send(err);
+          }
+          res.status(200).json(proveedor? {"Updated":true}: {"Updated":false});
+        });
     }
 }
